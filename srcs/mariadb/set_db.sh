09018da32fee8d636399a5498eba217782b96935
emenella <emenella@student.42lyon.fr>
2021-11-09 15:41:44 +0100

    finito

--- srcs/mariadb/set_db.sh
+++ srcs/mariadb/set_db.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-service mysql restart
+service mysql start
 echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root
 echo "CREATE USER '$MYSQL_USER'@'%';" | mysql -u root
 echo "SET password FOR '$MYSQL_USER'@'%' = password('$MYSQL_PASSWORD');" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-tail -f
--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -3,7 +3,7 @@
 # autoinstall of wordpress with wp-cli
 wp-cli --allow-root core download --path=/var/www/wordpress
 cp /wp-config.php /var/www/wordpress/wp-config.php
-
+chmod -R 644 /var/www/wordpress/wp-config.php
 
 wp-cli --allow-root core install --path=/var/www/wordpress --url=https://emenella.42.fr/ \
 --title="Inception" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=xxx@xxx.fr
@@ -11,6 +11,5 @@ wp-cli --allow-root core install --path=/var/www/wordpress --url=https://emenell
 # creating 2 wordpress users
 cd /home/script
 mysql --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE < ./create_user_wp.sql
-
 
 php-fpm7 -F
